7ece8fd10915696be9ed572e540749a45ba09bfa
Paula Pecker <paulapeckerfasce@gmail.com>
2023-08-22 12:44:01 -0300

    2

---
+++ ej2.py
@@ -0,0 +1,14 @@
+from ej1 import ndigits
+def invertir_rec(num): #asumo num entero positivo porque un lio
+
+    n = ndigits(num)
+    if n == 1:
+        return num
+    else:
+        num_aux=(num-int(num %10)-int(num/pow(10,n-1))*pow(10,n-1))/10
+        return int(num %10)*pow(10,n-1)+int(num/pow(10,n-1))+invertir_rec(num_aux)*10
+
+if __name__ =='__main__':
+    print(invertir_rec(9876))
+    print(invertir_rec(45189))
+    print(invertir_rec(2))
