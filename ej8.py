cbb7b90a9e3f01c7589334944f15710fcb91608a
Paula Pecker <paulapeckerfasce@gmail.com>
2023-08-23 13:23:45 -0300

    o factorial

--- ej8.py
+++ ej8.py
@@ -4,8 +4,21 @@ def C(total, grupo):
     else:
         return C(total-1,grupo-1)+C(total-1,grupo)
 
+def C_fact(total,grupo):
+    return fact(total)/(fact(grupo)*fact(total-grupo))
+
+def fact(n):
+    if n<=1:
+        return 1
+    else:
+        return n*fact(n-1)
 
 if __name__ == '__main__':
     print(C(5,3))
     print(C(9,4))
     print(C(15,6))
+    print(C(36,5))
+    print(C_fact(5, 3))
+    print(C_fact(9, 4))
+    print(C_fact(15, 6))
+    print(C_fact(36, 5))
